63c9a83bc8998184053fd428abc7126560fd33e1
steven burnett <stevendburnett@gmail.com>
2016-10-05 16:44:25 -0600

    will remove scheduler

--- Entry.js
+++ Entry.js
@@ -1,8 +1,5 @@
-const schedule = require('node-schedule')
 const LetsPlay = require('./LetsPlay')
 // const UtahSoccer = require('./UtahSoccer')
 
-schedule.scheduleJob('0 0 */3 * * *', () => {
-  LetsPlay.scrape()
-})
+LetsPlay.scrape()
 // UtahSoccer.scrape()
