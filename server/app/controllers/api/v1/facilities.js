57e9eb4ee44b6fa1e45da1fc345edadfa2cd3494
Felix Milea-Ciobanu <felixmilea@gmail.com>
2016-10-12 00:57:54 -0600

    refactored controller structure

--- server/app/controllers/api/v1/facilities.js
+++ server/app/controllers/api/v1/facilities.js
@@ -2,15 +2,15 @@ const async = require('asyncawait/async')
 const await = require('asyncawait/await')
 
 module.exports = () => ({
-  'v1/facilities/:facility/teams': {
-    get: function(req, res) {
+  ':facility/teams': {
+    get: (req, res, next) => {
       async(() => {
         try {
           const limit = Math.max(100, Math.min(1, parseInt(req.params.limit) || 50))
           let whereClause = {facilityId: req.params.facility}
           if (req.query.cursor) {
-            console.log(Buffer.from(req.query.cursor, 'base64').toString("utf-8"))
-            const cursor = JSON.parse(Buffer.from(req.query.cursor, 'base64').toString("utf-8"))
+            console.log(Buffer.from(req.query.cursor, 'base64').toString('utf-8'))
+            const cursor = JSON.parse(Buffer.from(req.query.cursor, 'base64').toString('utf-8'))
             whereClause = {
               $and: [whereClause,
                      {$or: [{name: {$gt: cursor.name}}, {name: cursor.name, teamId: {$gt: cursor.teamId}}]}
@@ -25,14 +25,13 @@ module.exports = () => ({
             // more pages. add pagination headers.
             const [fullUrl] = (req.protocol + '://' + req.get('host') + req.originalUrl).split('?') // take off the query string. todo: don't
             const lastTeam = teams[teams.length - 1]
-            const cursor = Buffer.from(JSON.stringify({name: lastTeam.name, teamId: lastTeam.teamId})).toString("base64")
+            const cursor = Buffer.from(JSON.stringify({name: lastTeam.name, teamId: lastTeam.teamId})).toString('base64')
             const queryString = `cursor=${cursor}`
             res.set('Link', `<${fullUrl}?${queryString}>; rel="next"`)
           }
           res.json(teams)
         } catch(e) {
-          console.log(e)
-          res.status(503).json({error: "couldn't get teams"})
+          next(e)
         }
       })()
     }
