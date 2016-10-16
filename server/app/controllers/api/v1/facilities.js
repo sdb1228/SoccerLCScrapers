9d7850ea1737456021fe0058dec0ea6e68a16273
Felix Milea-Ciobanu <felixmilea@gmail.com>
2016-10-15 23:32:44 -0600

    facilities tweaks

--- server/app/controllers/api/v1/facilities.js
+++ server/app/controllers/api/v1/facilities.js
@@ -13,57 +13,58 @@ module.exports = () => ({
           }
           const facilities = await(Models.Facility.findAll({
             order: ['name', 'id'],
-            limit: limit}))
-            if (facilities.length >= limit) {
-              // more pages. add pagination headers.
-              const [fullUrl] = (req.protocol + '://' + req.get('host') + req.originalUrl).split('?') // take off the query string. todo: don't
-              const lastFacility = facilities[facilities.length - 1]
-              const cursor = Buffer.from(JSON.stringify({name: lastFacility.name})).toString('base64')
-              const queryString = `cursor=${cursor}`
-              res.set('Link', `<${fullUrl}?${queryString}>; rel="next"`)
-            }
-            res.json(facilities)
-          } catch(e) {
-            next(e)
+            limit,
+          }))
+          if (facilities.length >= limit) {
+            // more pages. add pagination headers.
+            const [fullUrl] = (req.protocol + '://' + req.get('host') + req.originalUrl).split('?') // take off the query string. todo: don't
+            const lastFacility = facilities[facilities.length - 1]
+            const cursor = Buffer.from(JSON.stringify({name: lastFacility.name})).toString('base64')
+            const queryString = `cursor=${cursor}`
+            res.set('Link', `<${fullUrl}?${queryString}>; rel="next"`)
           }
-        })()
-      }
-    },
-
-    ':environment': {
-      get: (req, res, next) => {
-        async(() => {
-          try {
-            const limit = Math.max(100, Math.min(1, parseInt(req.params.limit) || 50))
-            let whereClause = {environment: req.params.environment}
-            if (req.query.cursor) {
-              console.log(Buffer.from(req.query.cursor, 'base64').toString('utf-8'))
-              const cursor = JSON.parse(Buffer.from(req.query.cursor, 'base64').toString('utf-8'))
-              whereClause = {
-                $and: [whereClause,
-                  {$or: [{name: {$gt: cursor.name}}, {name: cursor.name}]}
-                ]
-              }
-            }
-            const facilities = await(Models.Facility.findAll({
-              where: whereClause,
-              order: ['name', 'id'],
-              limit: limit}))
-              if (facilities.length >= limit) {
-                // more pages. add pagination headers.
-                const [fullUrl] = (req.protocol + '://' + req.get('host') + req.originalUrl).split('?') // take off the query string. todo: don't
-                const lastFacility = facilities[facilities.length - 1]
-                const cursor = Buffer.from(JSON.stringify({name: lastFacility.name})).toString('base64')
-                const queryString = `cursor=${cursor}`
-                res.set('Link', `<${fullUrl}?${queryString}>; rel="next"`)
-              }
-              res.json(facilities)
-            } catch(e) {
-              next(e)
-            }
-          })()
+          res.ok(facilities)
+        } catch(e) {
+          next(e)
         }
-      },
+      })()
+    },
+  },
+
+  ':environment': {
+    get: (req, res, next) => {
+      async(() => {
+        try {
+          const limit = Math.max(100, Math.min(1, parseInt(req.params.limit) || 50))
+          let whereClause = {environment: req.params.environment}
+          if (req.query.cursor) {
+            console.log(Buffer.from(req.query.cursor, 'base64').toString('utf-8'))
+            const cursor = JSON.parse(Buffer.from(req.query.cursor, 'base64').toString('utf-8'))
+            whereClause = {
+              $and: [whereClause,
+                {$or: [{name: {$gt: cursor.name}}, {name: cursor.name}]}
+              ]
+            }
+          }
+          const facilities = await(Models.Facility.findAll({
+            where: whereClause,
+            order: ['name', 'id'],
+            limit}))
+          if (facilities.length >= limit) {
+            // more pages. add pagination headers.
+            const [fullUrl] = (req.protocol + '://' + req.get('host') + req.originalUrl).split('?') // take off the query string. todo: don't
+            const lastFacility = facilities[facilities.length - 1]
+            const cursor = Buffer.from(JSON.stringify({name: lastFacility.name})).toString('base64')
+            const queryString = `cursor=${cursor}`
+            res.set('Link', `<${fullUrl}?${queryString}>; rel="next"`)
+          }
+          res.ok(facilities)
+        } catch(e) {
+          next(e)
+        }
+      })()
+    }
+  },
 
   ':facility/teams': {
     get: (req, res, next) => {
@@ -83,7 +84,7 @@ module.exports = () => ({
           const teams = await(Models.Team.findAll({
             where: whereClause,
             order: ['name', 'teamId', 'id'],
-            limit: limit}))
+            limit}))
           if (teams.length >= limit) {
             // more pages. add pagination headers.
             const [fullUrl] = (req.protocol + '://' + req.get('host') + req.originalUrl).split('?') // take off the query string. todo: don't
@@ -92,7 +93,7 @@ module.exports = () => ({
             const queryString = `cursor=${cursor}`
             res.set('Link', `<${fullUrl}?${queryString}>; rel="next"`)
           }
-          res.json(teams)
+          res.ok(teams)
         } catch(e) {
           next(e)
         }
@@ -117,24 +118,24 @@ module.exports = () => ({
             const cursor = JSON.parse(Buffer.from(req.query.cursor, 'base64').toString('utf-8'))
             whereClause = {
               $and: [whereClause,
-                     {$or: [{name: {$gt: cursor.name}}, {name: cursor.name, teamId: {$gt: cursor.gameId}}]}
+                     {$or: [{name: {$gt: cursor.name}}, {gameId: {$gt: cursor.gameId}}]}
                     ]
             }
           }
           const games = await(Models.Game.findAll({
             where: whereClause,
             order: ['gameDateTime', 'facilityId', 'id'],
-            limit: limit,
+            limit,
           }))
           if (games.length >= limit) {
             // more pages. add pagination headers.
             const [fullUrl] = (req.protocol + '://' + req.get('host') + req.originalUrl).split('?') // take off the query string. todo: don't
-            const lastTeam = teams[teams.length - 1]
-            const cursor = Buffer.from(JSON.stringify({name: lastTeam.name, teamId: lastTeam.teamId})).toString('base64')
+            const lastGame = games[games.length - 1]
+            const cursor = Buffer.from(JSON.stringify({gameId: lastGame.gameId})).toString('base64')
             const queryString = `cursor=${cursor}`
             res.set('Link', `<${fullUrl}?${queryString}>; rel="next"`)
           }
-          res.json(teams)
+          res.ok(games)
         } catch(e) {
           next(e)
         }
@@ -160,24 +161,24 @@ module.exports = () => ({
             const cursor = JSON.parse(Buffer.from(req.query.cursor, 'base64').toString('utf-8'))
             whereClause = {
               $and: [whereClause,
-                     {$or: [{name: {$gt: cursor.name}}, {name: cursor.name, teamId: {$gt: cursor.gameId}}]}
+                     {$or: [{name: {$gt: cursor.name}}, {gameId: {$gt: cursor.gameId}}]}
                     ]
             }
           }
           const games = await(Models.Game.findAll({
             where: whereClause,
             order: ['gameDateTime', 'facilityId', 'id'],
-            limit: limit,
+            limit,
           }))
           if (games.length >= limit) {
             // more pages. add pagination headers.
             const [fullUrl] = (req.protocol + '://' + req.get('host') + req.originalUrl).split('?') // take off the query string. todo: don't
-            const lastTeam = teams[teams.length - 1]
-            const cursor = Buffer.from(JSON.stringify({name: lastTeam.name, teamId: lastTeam.teamId})).toString('base64')
+            const lastGame = games[games.length - 1]
+            const cursor = Buffer.from(JSON.stringify({gameId: lastGame.gameId})).toString('base64')
             const queryString = `cursor=${cursor}`
             res.set('Link', `<${fullUrl}?${queryString}>; rel="next"`)
           }
-          res.json(teams)
+          res.ok(games)
         } catch(e) {
           next(e)
         }
